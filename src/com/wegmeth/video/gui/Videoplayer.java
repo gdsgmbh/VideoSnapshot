3153deceefaa541884f5f7870ea5ab4f13b14eb1
kerkhoff01 <kerkhoff01@DE-SG-PC-21246.technotrans.lan>
2016-08-23 11:59:18 +0200

    Der Dialog kann bei der QR-Code-Erstellung erst bestätigt werden, wenn das QR-Code-Feld überhaubt erst gefüllt ist.

--- src/com/wegmeth/video/gui/Videoplayer.java
+++ src/com/wegmeth/video/gui/Videoplayer.java
@@ -1,24 +1,27 @@
 package com.wegmeth.video.gui;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Scale;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Text;
-
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import org.eclipse.swt.widgets.Scale;
 
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
@@ -39,43 +42,43 @@ public class Videoplayer {
 	private Image imgPlay;
 	private Image imgPause;
 	private Button optionThumb;
-	
+	private Button optionQR;
 	
 	public void close(){
-		shell.dispose();
+		this.shell.dispose();
 	}
 	
 	/**
 	 * Main Dialog with all Controls and Options for Thumbnail
 	 */
 	public Videoplayer (){
-		shell = new Shell();
-		imgPlay= new Image(shell.getDisplay(),getClass().getResourceAsStream("/com/wegmeth/video/res/play.png"));
-		imgPause= new Image(shell.getDisplay(),getClass().getResourceAsStream("/com/wegmeth/video/res/pause.png"));
+		this.shell = new Shell();
+		this.imgPlay= new Image(this.shell.getDisplay(),getClass().getResourceAsStream("/com/wegmeth/video/res/play.png"));
+		this.imgPause= new Image(this.shell.getDisplay(),getClass().getResourceAsStream("/com/wegmeth/video/res/pause.png"));
 		
-		shell.setSize(600, 700);
-		shell.setLayout(new GridLayout(1, false));
+		this.shell.setSize(600, 700);
+		this.shell.setLayout(new GridLayout(1, false));
 		this.shell.addDisposeListener(new DisposeListener() {
 			@Override
 			public void widgetDisposed(DisposeEvent arg0) {
-				mediaPlayer.release();
+				Videoplayer.this.mediaPlayer.release();
 			}
 		});
 		
-		VideoCmp cmpVideo = new VideoCmp(shell, SWT.EMBEDDED);
-		mediaPlayer = cmpVideo.getPlayer();
+		VideoCmp cmpVideo = new VideoCmp(this.shell, SWT.EMBEDDED);
+		this.mediaPlayer = cmpVideo.getPlayer();
 		
-		Composite cmpControls = new Composite(shell, SWT.NONE);
+		Composite cmpControls = new Composite(this.shell, SWT.NONE);
 		cmpControls.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		cmpControls.setLayout(new GridLayout(1, false));
 		
-		Composite cmpOptions = new Composite(shell, SWT.NONE);
+		Composite cmpOptions = new Composite(this.shell, SWT.NONE);
 		GridData gd_cmpOptions = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		gd_cmpOptions.widthHint = 777;
 		cmpOptions.setLayoutData(gd_cmpOptions);
 		cmpOptions.setLayout(new GridLayout(1, false));
 		
-		final Scale scale = new Scale(cmpControls, SWT.NONE);
+		Scale scale = new Scale(cmpControls, SWT.NONE);
 		GridData gd_scale = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		gd_scale.widthHint = 767;
 		scale.setLayoutData(gd_scale);
@@ -86,7 +89,7 @@ public class Videoplayer {
 				int val = 100 - (scale.getMaximum()
 						- scale.getSelection() + scale
 						.getMinimum());
-				long max =mediaPlayer.getLength();
+				long max =Videoplayer.this.mediaPlayer.getLength();
 				Videoplayer.this.mediaPlayer.setTime(
 						(max * val / 100));
 			}
@@ -96,11 +99,11 @@ public class Videoplayer {
 		scale.setMaximum((int) this.mediaPlayer
 				.getLength());
 		scale.setPageIncrement(1);
-		run = new Runnable() {
+		this.run = new Runnable() {
 			@Override
 			public void run() {
 				long pos = Videoplayer.this.mediaPlayer.getTime();
-				long max = mediaPlayer.getLength();
+				long max = Videoplayer.this.mediaPlayer.getLength();
 				if (max == 0) {
 					return;
 				}
@@ -112,26 +115,26 @@ public class Videoplayer {
 						scale.setSelection((int) p);
 						int sec = (int) ((f / 1000) % 60);
 						int min = (int) ((f / 1000) / 60);
-						dateTime.setTime(0, min, sec);
+						Videoplayer.this.dateTime.setTime(0, min, sec);
 					}
 				}
 			}
 		};
 		
-		final Button btnPlay = new Button(cmpControls, SWT.NONE);
-		btnPlay.setImage(imgPause);
+		Button btnPlay = new Button(cmpControls, SWT.NONE);
+		btnPlay.setImage(this.imgPause);
 		btnPlay.setSize(75, 25);
 		
 		btnPlay.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				boolean isPlaying = mediaPlayer.isPlaying();
-				mediaPlayer.setPause(isPlaying);
+				boolean isPlaying = Videoplayer.this.mediaPlayer.isPlaying();
+				Videoplayer.this.mediaPlayer.setPause(isPlaying);
 				
 				if(!isPlaying){
-					btnPlay.setImage(imgPause);
+					btnPlay.setImage(Videoplayer.this.imgPause);
 				}else{
-					btnPlay.setImage(imgPlay);
+					btnPlay.setImage(Videoplayer.this.imgPlay);
 				}
 			}
 		});
@@ -156,10 +159,10 @@ public class Videoplayer {
 						.getHours() * 60 * 60)) * 1000;
 
 				if (milis <= Videoplayer.this.mediaPlayer.getLength()) {
-					mediaPlayer
+					Videoplayer.this.mediaPlayer
 							.setTime(milis);
-					long pos =mediaPlayer.getTime();
-					long max = mediaPlayer.getLength();
+					long pos =Videoplayer.this.mediaPlayer.getTime();
+					long max = Videoplayer.this.mediaPlayer.getLength();
 					float f = (pos % max);
 					float p = (f / max) * 100;
 					scale.setSelection((int) p);
@@ -167,38 +170,73 @@ public class Videoplayer {
 			}
 		});
 		
-		optionThumb = new Button(cmpOptions, SWT.RADIO);
-		optionThumb.setText("Standbild erstellen");
-		optionThumb.setSelection(true);
+		this.optionThumb = new Button(cmpOptions, SWT.RADIO);
+		this.optionThumb.setText("Standbild erstellen");
+		this.optionThumb.setSelection(true);
 		
-		Button optionQR = new Button(cmpOptions, SWT.RADIO);
-		optionQR.setText("QR-Code erzeugen");
+		this.optionQR = new Button(cmpOptions, SWT.RADIO);
+		this.optionQR.setText("QR-Code erzeugen");
 		
-		qrURL = new Text(cmpOptions, SWT.BORDER|SWT.SEARCH);
-		qrURL.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		qrURL.setMessage("Video URL");
+		// Wenn QR-Code ausgewählt wurde, wird das QR-Code-Feld freigegeben
+		// Des Weiteren wird geprueft, ob das Textfeld leer ist.
+		// Ist dies der Fall, wird der OK-Button solange disabled
+		this.optionQR.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				if(Videoplayer.this.optionQR.getSelection()){
+					Videoplayer.this.qrURL.setEnabled(true);
+					Videoplayer.this.btnOk.setEnabled(!Videoplayer.this.qrURL.getText().equals(""));
+				}
+				else{
+					Videoplayer.this.qrURL.setEnabled(false);
+					Videoplayer.this.btnOk.setEnabled(true);
+				}
+							
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
-		Composite cmpButtons = new Composite(shell, SWT.NONE);
+		this.qrURL = new Text(cmpOptions, SWT.BORDER|SWT.SEARCH);
+		this.qrURL.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		this.qrURL.setMessage("Video URL");
+
+		// Wenn der Text sich aendert und QR-Code ausgewaehlt wurde, darf der OK-Button nur aktiv sein, wenn etwas eingetragen wurde.
+		this.qrURL.addModifyListener(new ModifyListener() {
+			
+			@Override
+			public void modifyText(ModifyEvent arg0) {
+				if(Videoplayer.this.optionQR.getSelection())
+					Videoplayer.this.btnOk.setEnabled(!Videoplayer.this.qrURL.getText().equals(""));
+			}
+		});
+		
+		Composite cmpButtons = new Composite(this.shell, SWT.NONE);
 		GridData gd_cmpButtons = new GridData(SWT.RIGHT, SWT.BOTTOM, true, true, 1, 1);
 		gd_cmpButtons.widthHint = 120;
 		cmpButtons.setLayoutData(gd_cmpButtons);
 		cmpButtons.setLayout(new GridLayout(2, false));
 		
-		btnCancel = new Button(cmpButtons, SWT.RIGHT);
-		btnCancel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
-		btnCancel.setAlignment(SWT.CENTER);
-		btnCancel.setText("Abbrechen");
+		this.btnCancel = new Button(cmpButtons, SWT.RIGHT);
+		this.btnCancel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		this.btnCancel.setAlignment(SWT.CENTER);
+		this.btnCancel.setText("Abbrechen");
 		
-		btnOk = new Button(cmpButtons, SWT.CENTER);
-		btnOk.setText("OK");
+		this.btnOk = new Button(cmpButtons, SWT.CENTER);
+		this.btnOk.setText("OK");
 	}
 	
 	public void open(){
-		shell.open();
-		shell.setVisible(true);
-		while (!shell.isDisposed()) {
+		this.shell.open();
+		this.shell.setVisible(true);
+		while (!this.shell.isDisposed()) {
 			syncScaler();
-			if (!shell.getDisplay().readAndDispatch()) {
+			if (!this.shell.getDisplay().readAndDispatch()) {
 				this.shell.getDisplay().sleep();
 				try {
 					Thread.sleep(100);
@@ -210,8 +248,8 @@ public class Videoplayer {
 	}
 	
 	public void syncScaler() {
-		if (mediaPlayer.isPlaying()) {
-			shell.getDisplay().syncExec(this.run);
+		if (this.mediaPlayer.isPlaying()) {
+			this.shell.getDisplay().syncExec(this.run);
 		}
 	}
 	
@@ -219,29 +257,29 @@ public class Videoplayer {
 	 * @param adapter what happens when dialog is ok
 	 */
 	public void addOkAdapter(SelectionAdapter adapter){
-		btnOk.addSelectionListener(adapter);
+		this.btnOk.addSelectionListener(adapter);
 	}
 	
 	/**
 	 * @param adapter What happend if dialog is canceled
 	 */
 	public void addCancelAdapter(SelectionAdapter adapter){
-		btnCancel.addSelectionListener(adapter);
+		this.btnCancel.addSelectionListener(adapter);
 	}
 	
 	public boolean isSnap(){
-		return optionThumb.getSelection();
+		return this.optionThumb.getSelection();
 	}
 
 	public java.awt.Image getSnap() {
-		return mediaPlayer.getSnapshot();
+		return this.mediaPlayer.getSnapshot();
 	}
 
 	public String getQRURL() {
-		return qrURL.getText();
+		return this.qrURL.getText();
 	}
 
 	public void play(String mediaPath) {
-		mediaPlayer.playMedia(mediaPath);
+		this.mediaPlayer.playMedia(mediaPath);
 	}
 }
